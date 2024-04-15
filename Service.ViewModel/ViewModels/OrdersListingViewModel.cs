e747a4e01ad91d9e36a68ba3fa025f7353103d90
Arekd125 <arekdom125@gmail.com>
2024-04-15 19:01:08 +0200

    add orders to ordersListingViewModel

--- Service.ViewModel/ViewModels/OrdersListingViewModel.cs
+++ Service.ViewModel/ViewModels/OrdersListingViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Servis.Models.OrderModels;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
@@ -7,14 +8,18 @@ using System.Threading.Tasks;
 
 namespace Service.ViewModel.ViewModels
 {
-    public class OrdersListingViewModel
+    public class OrdersListingViewModel : ViewModelBase
     {
         private ObservableCollection<OrdersViewModel> _ordersViewModelColection;
+        private readonly List<Order> _orders;
 
         public IEnumerable<OrdersViewModel> ordersViewModel => _ordersViewModelColection;
 
-        public OrdersListingViewModel()
+        public OrdersListingViewModel(List<Order> orders)
         {
+            _ordersViewModelColection = new ObservableCollection<OrdersViewModel>();
+
+            _orders = orders;
         }
     }
 }
