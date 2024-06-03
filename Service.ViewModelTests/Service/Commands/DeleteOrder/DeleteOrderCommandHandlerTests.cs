4d0cf4d7b7b0e8c23c43ff42524bcc907f40d0b0
Arekd125 <arekdom125@gmail.com>
2024-06-03 15:55:04 +0200

    add DeleteOrderTests

---
+++ Service.ViewModelTests/Service/Commands/DeleteOrder/DeleteOrderCommandHandlerTests.cs
@@ -0,0 +1,82 @@
+﻿using Xunit;
+using Service.ViewModel.Service.Commands.DeleteOrder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using MediatR;
+using Moq;
+using Service.Model.Repositories;
+using Servis.Models.OrderModels;
+
+namespace Service.ViewModel.Service.Commands.DeleteOrder.Tests
+{
+    public class DeleteOrderCommandHandlerTests
+    {
+        private readonly Mock<IOrderRepository> _orderRepositoryMock;
+        private readonly Mock<IContactRepository> _contactRepositoryMock;
+        private readonly DeleteOrderCommandHandler _handler;
+
+        public DeleteOrderCommandHandlerTests()
+        {
+            _orderRepositoryMock = new Mock<IOrderRepository>();
+            _contactRepositoryMock = new Mock<IContactRepository>();
+            _handler = new DeleteOrderCommandHandler(_orderRepositoryMock.Object, _contactRepositoryMock.Object);
+        }
+
+        [Fact]
+        public async Task Handle_OrderDoesNotExist_ReturnsUnitValue()
+        {
+            // Arrange
+            var command = new DeleteOrderCommand("NonExistentOrder");
+            _orderRepositoryMock.Setup(repo => repo.GetOrderByOrderName(It.IsAny<string>())).ReturnsAsync((Order)null);
+
+            // Act
+            var result = await _handler.Handle(command, CancellationToken.None);
+
+            // Assert
+            result.Should().Be(Unit.Value);
+        }
+
+        [Fact]
+        public async Task Handle_OrderExists_ContactUsedByOtherOrders_DoesNotDeleteContact()
+        {
+            // Arrange
+            var command = new DeleteOrderCommand("ExistingOrder");
+            var contact = new Contact { Id = 1, Name = "NameTest", PhoneNumber = "123 456 890" };
+            var order = new Order { OrderName = "ExistingOrder", Contact = contact, ContactId = contact.Id };
+
+            _orderRepositoryMock.Setup(repo => repo.GetOrderByOrderName(It.IsAny<string>())).ReturnsAsync(order);
+            _orderRepositoryMock.Setup(repo => repo.AnyOrderWithContactId(contact.Id)).ReturnsAsync(true);
+
+            // Act
+            var result = await _handler.Handle(command, CancellationToken.None);
+
+            // Assert
+            result.Should().Be(Unit.Value);
+            _contactRepositoryMock.Verify(repo => repo.Delete(It.IsAny<Contact>()), Times.Never);
+        }
+
+        [Fact]
+        public async Task Handle_OrderExists_ContactNotUsedByOtherOrders_DeletesContact()
+        {
+            // Arrange
+            var command = new DeleteOrderCommand("ExistingOrder");
+            var contact = new Contact { Id = 1, Name = "NameTest", PhoneNumber = "123 456 890" };
+            var order = new Order { OrderName = "ExistingOrder", Contact = contact, ContactId = contact.Id };
+
+            _orderRepositoryMock.Setup(repo => repo.GetOrderByOrderName(It.IsAny<string>())).ReturnsAsync(order);
+            _orderRepositoryMock.Setup(repo => repo.AnyOrderWithContactId(contact.Id)).ReturnsAsync(false);
+            _contactRepositoryMock.Setup(repo => repo.Delete(contact)).Returns(Task.CompletedTask);
+
+            // Act
+            var result = await _handler.Handle(command, CancellationToken.None);
+
+            // Assert
+            result.Should().Be(Unit.Value);
+            _contactRepositoryMock.Verify(repo => repo.Delete(contact), Times.Once);
+        }
+    }
+}
