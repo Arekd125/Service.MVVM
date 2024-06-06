763f0c98a98c4e337163d3ec39cc1dc66db7e8e1
Arekd125 <arekdom125@gmail.com>
2024-06-06 18:54:19 +0200

    add colaps orderDetails

---
+++ Service.View/Views/EmptyToDoVisibiltyConverter.cs
@@ -0,0 +1,33 @@
+﻿using Service.Model.Entity;
+using Service.ViewModel.Dtos;
+using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Service.View.Views
+{
+    public class EmptyToDoVisibiltyConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+            {
+                return Visibility.Collapsed;
+            }
+
+            IEnumerable<ToDoDto> toDo = value as IEnumerable<ToDoDto>;
+            return toDo.Any() && toDo != null ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
