c53be8d78d458e8c8d1cc7e20b328784e7132caa
Arekd125 <arekdom125@gmail.com>
2024-06-10 15:34:15 +0200

    ref

--- Service.ViewModel/ViewModels/OrdersListingViewModel.cs
+++ Service.ViewModel/ViewModels/OrdersListingViewModel.cs
@@ -17,7 +17,7 @@ namespace Service.ViewModel.ViewModels
     public class OrdersListingViewModel : ViewModelBase
     {
         public IDialogCoordinator _dialogCoordinator;
-        private ObservableCollection<OrderDto> _ordersViewModelCollection;
+        private ObservableCollection<OrderDto> _ordersCollection;
         private readonly OrderStore _orderStore;
         private readonly IMediator _mediator;
 
@@ -27,7 +27,7 @@ namespace Service.ViewModel.ViewModels
         public ICommand DeleteOrderButton { get; }
         public ICommand EditOrderButton { get; }
 
-        public ObservableCollection<OrderDto> OrdersViewModelCollection => _ordersViewModelCollection;
+        public ObservableCollection<OrderDto> OrdersViewModelCollection => _ordersCollection;
 
         private int _ordersViewModelSelectedIndex = -1;
 
@@ -46,52 +46,52 @@ namespace Service.ViewModel.ViewModels
 
         public OrdersListingViewModel(OrderStore orderStore, IMediator mediator, IDialogCoordinator dialogCoordinator, IMapper mapper)
         {
-            _ordersViewModelCollection = new ObservableCollection<OrderDto>();
+            _ordersCollection = new ObservableCollection<OrderDto>();
             _orderStore = orderStore;
             _mediator = mediator;
+            _dialogCoordinator = dialogCoordinator;
+
             _orderStore.OrderCreated += OnOrderCreated;
             _orderStore.OrderEdited += OnOrderEdited;
-            _dialogCoordinator = dialogCoordinator;
+
             EditStatusButton = new EditStatusButtonCommand(this);
             EditOrderButton = new EditOrderButtonCommand(this, _orderStore);
             DeleteOrderButton = new DeleteOrderButtonCommand(this);
-            AllOrders();
+            LoadAllOrders();
         }
 
         private void OnOrderCreated(OrderDto orderDto)
         {
-            Add(orderDto);
+            AddOrder(orderDto);
         }
 
         public void EditStatusOrder()
         {
-          
             var OrderToEdit = GetOrderByIndex(OrdersViewModelSelectedIndex);
-           _ordersViewModelCollection[EditOrderIndex].IsFinished = !_ordersViewModelCollection[EditOrderIndex].IsFinished;
+            _ordersCollection[EditOrderIndex].IsFinished = !_ordersCollection[EditOrderIndex].IsFinished;
             _mediator.Send(new EditOrderStatusCommand(OrderToEdit.OrderName));
-          
         }
 
         private void OnOrderEdited(OrderDto orderDto)
         {
-           var isFinished = _ordersViewModelCollection[EditOrderIndex].IsFinished;
-            _ordersViewModelCollection.RemoveAt(EditOrderIndex);
+            var isFinished = _ordersCollection[EditOrderIndex].IsFinished;
+            _ordersCollection.RemoveAt(EditOrderIndex);
             orderDto.IsFinished = isFinished;
-            _ordersViewModelCollection.Insert(EditOrderIndex, orderDto);
+            _ordersCollection.Insert(EditOrderIndex, orderDto);
         }
 
-        private void Add(OrderDto orderDto)
+        private void AddOrder(OrderDto orderDto)
         {
-            _ordersViewModelCollection.Insert(0, orderDto);
+            _ordersCollection.Insert(0, orderDto);
         }
 
-        private void AllOrders()
+        private void LoadAllOrders()
         {
             var getAllOrders = _mediator.Send(new GetAllOrdersQuery()).Result;
 
             foreach (var o in getAllOrders)
             {
-                Add(o);
+                AddOrder(o);
             }
         }
 
@@ -116,7 +116,6 @@ namespace Service.ViewModel.ViewModels
             };
             var selectedOrder = GetOrderByIndex(index);
             var title = $"Usunąć Zlecenie {selectedOrder.OrderName}?";
-            //  var models = string.Join(", ", ModelStateNameItemSorce);
             var message = $"Imię i Nazwisko: {selectedOrder.ContactName}\nTelefone: {selectedOrder.ContactPhoneNumber}\nMarka i Model: {selectedOrder.Device} {selectedOrder.Model}";
 
             MessageDialogResult result = await _dialogCoordinator.ShowMessageAsync(
@@ -128,19 +127,20 @@ namespace Service.ViewModel.ViewModels
 
             if (result == MessageDialogResult.Affirmative)
             {
-                DeleteOrder(index, selectedOrder.OrderName);
+                DeleteOrder(index);
             }
         }
 
         public OrderDto GetOrderByIndex(int index)
         {
-            return _ordersViewModelCollection[index];
+            return _ordersCollection[index];
         }
 
-        public void DeleteOrder(int index, string OrderName)
+        public void DeleteOrder(int index)
         {
-            _ordersViewModelCollection.RemoveAt(index);
-            _mediator.Send(new DeleteOrderCommand(OrderName));
+            var orderToDelete = GetOrderByIndex(index);
+            _ordersCollection.RemoveAt(index);
+            _mediator.Send(new DeleteOrderCommand(orderToDelete.OrderName));
         }
     }
 }
