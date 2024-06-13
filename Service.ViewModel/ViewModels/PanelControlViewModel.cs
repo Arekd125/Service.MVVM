4b685b81de8301cf2cd8a0752432ce05eba6169e
Arekd125 <arekdom125@gmail.com>
2024-06-13 22:14:07 +0200

    add enum ItemSorce

--- Service.ViewModel/ViewModels/PanelControlViewModel.cs
+++ Service.ViewModel/ViewModels/PanelControlViewModel.cs
@@ -1,12 +1,42 @@
-﻿using System;
+﻿using Service.ViewModel.Stores.Converstes;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Service.ViewModel.ViewModels
 {
-    public class PanelControlViewModel
+   
+    [TypeConverter(typeof(EnumDescriptionTypeConverter))]
+    public enum DateFilerEnum 
     {
+        [Description("Dzisja")]
+        today,
+        [Description("Wczoraj")]
+        yesterday,
+        [Description("Tyedzień")]
+        week,
+        [Description("Miesiąc")]
+        month,
+        [Description("Ostatnie 30 dni")]
+        last_30Days,
+        [Description("Od początku")]
+        from_the_eginning,
+
+    };
+
+    public class PanelControlViewModel : ViewModelBase
+    {
+
+        public IEnumerable<DateFilerEnum> DateFilerItemSource => Enum.GetValues<DateFilerEnum>();
+       
+
+
+
+
     }
 }
