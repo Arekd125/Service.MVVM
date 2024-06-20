b940ef92483667ce383ddc3bad8438b4a24076e4
Arekd125 <arekdom125@gmail.com>
2024-06-20 12:36:34 +0200

    add second column of ToDos

--- Service.ViewModel/ViewModels/PrintOrderViewModels/PrintViewModel.cs
+++ Service.ViewModel/ViewModels/PrintOrderViewModels/PrintViewModel.cs
@@ -10,16 +10,6 @@ namespace Service.ViewModel.ViewModels.PrintOrderViewModels
         public Inprint inprint { get; }
         public string rodo { get; }
 
-        public PrintViewModel()
-        {
-            inprint = new Inprint("AD-KOMP Arkadius Dominiak",
-             "Szarych Szeregów 3", "Konin", "62-500", "534 078 017", "534 078 018", null);
-
-            rodo = @$"W związku z realizacją niniejszego zlecenia, informujemy,że w celu jego wykonania niezbędne jest przetwarzanie Państwa danych osobowych.
-Administratorem danych osobowych jest {inprint.Name} z siedzibą w {inprint.City}ie {inprint.Street}.
-Ja, niżej podpisany/a, wyrażam zgodę na przetwarzanie moich danych osobowych przez {inprint.Name} z siedzibą w {inprint.City}ie w celu realizacji zlecenia.";
-        }
-
         public OrderDto Order
         {
             get
@@ -30,6 +20,42 @@ Ja, niżej podpisany/a, wyrażam zgodę na przetwarzanie moich danych osobowych 
             {
                 _orderDto = value;
                 OnPropertyChanged(nameof(Order));
+                ToDoSpliter();
+            }
+        }
+
+        private IEnumerable<ToDoDto> _todo2;
+
+        public IEnumerable<ToDoDto> ToDo2
+        {
+            get
+            {
+                return _todo2;
+            }
+            set
+            {
+                _todo2 = value;
+                OnPropertyChanged(nameof(ToDo2));
+            }
+        }
+
+        public PrintViewModel()
+        {
+            inprint = new Inprint("AD-KOMP Arkadius Dominiak",
+             "Szarych Szeregów 3", "Konin", "62-500", "534 078 017", "534 078 018", null);
+
+            rodo = @$"W związku z realizacją niniejszego zlecenia, informujemy,że w celu jego wykonania niezbędne jest przetwarzanie Państwa danych osobowych.
+Administratorem danych osobowych jest {inprint.Name} z siedzibą w {inprint.City}ie {inprint.Street}.
+Ja, niżej podpisany/a, wyrażam zgodę na przetwarzanie moich danych osobowych przez {inprint.Name} z siedzibą w {inprint.City}ie w celu realizacji zlecenia.";
+        }
+
+        private void ToDoSpliter()
+        {
+            _todo2 = new List<ToDoDto>();
+            if (Order.ToDo.Count() > 5)
+            {
+                ToDo2 = _orderDto.ToDo.Skip(5).ToList();
+                Order.ToDo = _orderDto.ToDo.Take(5).ToList();
             }
         }
     }
